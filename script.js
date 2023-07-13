74d10a79c9b71fd746ad7656344801005839f98c
guilherme-liveseo <guilherme@liveseo.com.br>
2023-07-13 20:33:41 -0300

    Terminando o projeto

--- script.js
+++ script.js
@@ -1,4 +1,89 @@
-let a = "12";
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag,firstScriptTag);
 
-console.log(a);
+
+let video;
+let ambientLight;
+const videoId = 'qC0vDKVPCrw';
+
+
+
+function createAmbientLight() {
+    if(!animationHasEnded) return
+
+    ambientLight = new YT.Player("ambient-light", {
+        videoId,
+        events: {
+            onReady: ambientLightReady,
+            onStateChange: ambientStateChange,
+        }
+    })
+}
+
+
+window.onYouTubeIframeAPIReady = function (){
+    video = new YT.Player('video', {
+        videoId,
+        events: {
+            onStateChange: videoStateChange,
+        },
+    })
+
+}
+
+
+function videoStateChange (event) {
+    switch(event.data){
+        case YT.PlayerState.PLAYING:
+            if(!ambientLight) return;
+            ambientLight.seekTo(event.target.getCurrentTime());
+            ambientLight.playVideo();
+
+            break;
+        case YT.PlayerState.PAUSE:
+            if(!ambientLight) return;
+            ambientLight.seekTo(event.target.getCurrentTime());
+            ambientLight.pauseVideo();
+
+            break;
+    }
+}
+
+function betterAmbientLight(event) {
+    event.target.mute();
+
+    const qualityLevels = event.target.getAvailableQualityLevels();
+    if(qualityLevels && qualityLevels.lenght && qualityLevels.lenght > 0) {
+        qualityLevels.reverse();
+
+        console.log(qualityLevels);
+    }
+
+
+}
+function ambientLightReady(event) {
+    betterAmbientLight(event);
+
+}
+function ambientStateChange(event) {
+    switch(event.data){
+        case YT.PlayerState.BUFFERING:
+
+        case YT.PlayerState.PLAYING:
+            betterAmbientLight(event)
+    }
+}
+
+
+const app = document.querySelector("#app");
+app.addEventListener('animationend', e => {
+    if(e.animationName != 'appear') return;
+
+    animationHasEnded = true;
+    
+    createAmbientLight();
+})
+
 
